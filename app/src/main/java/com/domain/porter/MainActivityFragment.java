d87a7851b12cb02c067515c7a22f63e5104b07d5
Pratik Todi <ptodi.y12@lnmiit.ac.in>
2015-08-30 04:07:20 +0530

    removed unused import

--- app/src/main/java/com/domain/porter/MainActivityFragment.java
+++ app/src/main/java/com/domain/porter/MainActivityFragment.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
